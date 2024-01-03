7512502b5abe4038a862b180229d227baefb8a5d
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-01-03 12:26:23 -0800

    a function that prints a string in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,10 @@
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("holberton")
-uppercase("Holberton School 98 Battery street")
+#!/usr/bin/python3
+def uppercase(str):
+    upper = 0
+    for c in str:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            upper = 32
+        else:
+            upper = 0
+        print("{:c}".format(ord(c) - upper), end='')
+    print("")
