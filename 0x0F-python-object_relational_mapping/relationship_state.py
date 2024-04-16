4200af795e68fab9b58da279a2eb81e96a7f7c64
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-04-16 03:42:59 +0200

    Fixing Bugs

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """Lists states"""
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from relationship_state import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
 
-class City(Base):
+Base = declarative_base()
+
+
+class State(Base):
     """Class representing the states table"""
-    __tablename__ = 'cities'
+    __tablename__ = 'states'
 
     id = Column(Integer, nullable=False, primary_key=True,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
+    cities = relationship(
+        "City",
+        cascade="all, delete-orphan",
+        backref=backref("state", cascade="all"),
+        single_parent=True)
